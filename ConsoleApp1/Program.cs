23c3f71d658cf7e4e8ea3734539e6f725a7fb61f
Szabolcs Gaspar <396kij@gmail.com>
2020-10-07 11:22:45 +0200

    asd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,46 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static Random r = new Random();
+        static int[] tomb = new int[100];
+
         static void Main(string[] args)
         {
+            feltolt();
+            kiir();
+            for (int i = 0; i < 100; i++)
+            {
+                RandomHelyreIr();
+            }
 
-
-
-
-
+            Console.ReadKey();
         }
+        static void feltolt()
+        {
+            
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = r.Next(10, 100);
+            }
+        }
+        static void kiir()
+        {
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write($"{tomb[i]} ");
+                if ((i + 1) % 10 == 0) Console.Write("\n");   
+            }
+        }
+        static void RandomHelyreIr()
+        {
+            
+            Console.SetCursorPosition(
+                 r.Next(Console.WindowWidth) , r.Next(Console.WindowHeight)
+                );
+            Console.Write("*");
+        }
+
     }
 }
